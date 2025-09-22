cb5837dbe6b58d13f1949655f76cbdcc0fcecd6f
Crazy-Bird-Hypno <manvog72@googlemail.com>
2025-09-22 19:57:31 +0200

    Update CheckIcon.tsx

--- src/components/icons/CheckIcon.tsx
+++ src/components/icons/CheckIcon.tsx
@@ -1,19 +1,16 @@
 import React from 'react';
 
-const CheckIcon = (props: React.SVGProps<SVGSVGElement>) => (
+const CheckIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24 24"
     fill="none"
+    viewBox="0 0 24 24"
     stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
+    strokeWidth={2}
   >
-    <path d="M20 6 9 17l-5-5" />
+    <path strokeLinecap="round" strokeLinejoin="round" d="M5 13l4 4L19 7" />
   </svg>
 );
 
