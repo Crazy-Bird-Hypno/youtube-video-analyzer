9680933fbbfcb6e6f9308c11390185948e3d2872
Crazy-Bird-Hypno <manvog72@googlemail.com>
2025-09-22 19:57:49 +0200

    Update ClipboardIcon.tsx

--- src/components/icons/ClipboardIcon.tsx
+++ src/components/icons/ClipboardIcon.tsx
@@ -1,20 +1,20 @@
 import React from 'react';
 
-const ClipboardIcon = (props: React.SVGProps<SVGSVGElement>) => (
+const ClipboardIcon = () => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="24"
     height="24"
-    viewBox="0 0 24 24"
     fill="none"
+    viewBox="0 0 24 24"
     stroke="currentColor"
-    strokeWidth="2"
-    strokeLinecap="round"
-    strokeLinejoin="round"
-    {...props}
+    strokeWidth={2}
   >
-    <rect width="8" height="4" x="8" y="2" rx="1" ry="1" />
-    <path d="M16 4h2a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2H6a2 2 0 0 1-2-2V6a2 2 0 0 1 2-2h2" />
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z"
+    />
   </svg>
 );
 
